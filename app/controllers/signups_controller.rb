e4c65bb7db7506096912b2daf907a42ed3a30def
Henry Miller <mail.henrym+git@gmail.com>
2022-03-17 11:50:34 -0400

    finished deliverables

---
+++ app/controllers/signups_controller.rb
@@ -0,0 +1,24 @@
+class SignupsController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+
+    def create
+        signup = Signup.create!(signup_params)
+        render json: signup.activity, status: :created
+    end
+
+    private
+
+    def signup_params
+        params.permit(:activity_id, :camper_id, :time)
+    end
+
+    def render_not_found_response
+        render json: { error: "Camper not found" }, status: :not_found
+    end
+    
+    def render_unprocessable_entity_response(invalid)
+        render json: {errors: invalid.record.errors.full_messages}, status: :unprocessable_entity
+    end
+
+end
